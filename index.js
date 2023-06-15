16d0166196adf3f59b916a55661d3331e919a595
Scott <scott.ph.chou@gmail.com>
2023-06-15 13:39:38 +0800

    feat: support personalMessage for SCW

--- index.js
+++ index.js
@@ -38,11 +38,20 @@ module.exports = class DappAuth {
       // try smart-contract wallet
       const erc1271CoreContract = new this.web3.eth.Contract(ERC1271, address);
 
+      // check persoonalSign hash first
       const magicValue = await erc1271CoreContract.methods
-        .isValidSignature(this._hashSCMessage(challenge), signature)
+        .isValidSignature(eoaChallengeHash, signature)
         .call();
+      if (magicValue === ERC1271_MAGIC_VALUE) {
+        return true;
+      } else {
+        // then check SCMessage hash (ERC-1654)
+        const magicValue2 = await erc1271CoreContract.methods
+          .isValidSignature(this._hashSCMessage(challenge), signature)
+          .call();
 
-      return magicValue === ERC1271_MAGIC_VALUE;
+        return magicValue2 === ERC1271_MAGIC_VALUE;
+      }
     } catch (err) {
       throw utils.mergeErrors(errEOA, err);
     }
